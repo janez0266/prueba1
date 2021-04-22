93f5466cecb22d570f1edd0a7427f318436c9137
Julio Añez <janez0266@gmail.com>
2021-04-22 13:26:51 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,6 @@
+// console.log("hello world")
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    console.log('click...');
+    document.querySelector('.nav-menu').classList.toggle('show');
+
+});
